e436d2213f2c932602c3f3413cf479607bcb03fc
Fred Schoeneman <fred.schoeneman@gmail.com>
2010-08-18 02:00:25 -0700

    Initial commit of unmodified new Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Handsomefencer::Application.config.secret_token = '35dc94bc0e9c1e8aa1ab7e385bad6ab8c1a3078e7f788cb1717fea7fb8f4be69ba0ad8c54ed39ec3b3e16447cb569d3d1e43e9d1c3ac372157b826c304146ee2'
